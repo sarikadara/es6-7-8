5068fa6b2a229cc83d55e77a7fa203e0bf7bc861
sarikadara <sarika.dara96@gmail.com>
2020-06-10 16:13:08 +0530

    params nd map

---
+++ main.js
@@ -0,0 +1,78 @@
+"use strict"
+/*
+function greet($greeting = 'Hello World'){
+  console.log($greeting);
+}
+greet();
+
+
+let args1 = [1,2,3];
+let args2 = [4,5,6];
+
+function test(){
+  console.log(args1+','+args2);
+}
+
+//test.apply(null, args);
+test(...args1,...args2);
+*/
+
+
+/*
+let myArray = [11,22,34,65,34];
+let mySet = new Set(myArray);
+mySet.add('100');
+mySet.add({a: 1, b: 2});
+mySet.delete(22);
+mySet.add('100');
+mySet.add('200');
+//console.log(mySet.size);
+mySet.forEach(function(val){
+  console.log(val);
+});
+
+let myMap = new Map([['a1','Hello'], ['b2', 'Goodbye']]);
+myMap.set('c3', 'Foo');
+myMap.delete('a1');
+console.log(myMap.size);
+let carWeakSet = new WeakSet();
+let car1 = {
+  make: 'Honda',
+  model: 'Civic'
+}
+carWeakSet.add(car1);
+let car2 = {
+  make: 'Toyota',
+  model: 'Camry'
+}
+carWeakSet.add(car2);
+carWeakSet.delete(car1);
+console.log(carWeakSet);
+*/
+
+let carWeakMap = new WeakMap();
+
+let key1 = {
+  id: 1
+}
+
+let car1 = {
+  make: 'Honda',
+  model: 'Civic'
+}
+
+let key2 = {
+  id: 2
+}
+
+let car2 = {
+  make: 'Toyota',
+  model: 'Camry'
+}
+
+carWeakMap.set(key1, car1);
+carWeakMap.set(key2, car2);
+
+carWeakMap.delete(key1);
+
+console.log(carWeakMap);
